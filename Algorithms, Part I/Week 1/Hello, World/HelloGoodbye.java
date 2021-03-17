503a7f31384ab95cad8b4164611d01c829f15ac3
Vasiliy Azarov <vasiliyazarov1997@gmail.com>
2021-03-17 12:00:54 +0300

    feat: add HelloGoodbye.java

---
+++ Algorithms, Part I/Week 1/Hello, World/HelloGoodbye.java
@@ -0,0 +1,13 @@
+/**
+ * This class print "Hello" (in straight order) and "Goodbye"
+ * (in reversed order) for two program arguments.
+ */
+class HelloGoodbye {
+    /**
+     * @param args read only first two arguments.
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hello %s and %s.\n", args[0], args[1]);
+        System.out.printf("Goodbye %s and %s.\n", args[1], args[0]);
+    }
+}
