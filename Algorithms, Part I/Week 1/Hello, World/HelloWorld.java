c34c3be4906bf0706d32af0fd98b08358abc95de
Vasiliy Azarov <vasiliyazarov1997@gmail.com>
2021-03-17 13:19:32 +0300

    feat(assignment): add Hello, World (close #5)

---
+++ Algorithms, Part I/Week 1/Hello, World/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Print "Hello, World" int <code>System.out</code> (console).
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
---
+++ Algorithms, Part I/Week 1/Hello, World/RandomWord.java
@@ -0,0 +1,23 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * This class reads a sequence of words from standard input and prints one of
+ * those words uniformly at random. It do not store the words in an array or
+ * list. It uses use Knuth’s method: when reading the i-th word, select it with
+ * probability 1/i to be the champion, replacing the previous champion. After
+ * reading all of the words, print the surviving champion.
+ */
+public class RandomWord {
+    public static void main(String[] args) {
+        String result = "";
+        for (int i = 1; !StdIn.isEmpty(); i++) {
+            String line = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / i)) {
+                result = line;
+            }
+        }
+        StdOut.println(result);
+    }
+}
