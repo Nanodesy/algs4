6fc4c4af45b7e3f117445e1526f284e85d86e001
Vasiliy Azarov <vasiliyazarov1997@gmail.com>
2021-03-17 11:30:05 +0300

    feat: add HelloWorld.java

---
+++ Algorithms, Part I/Week 1/Hello, World/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Print "Hello, World" int <code>System.out</code> (console).
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
